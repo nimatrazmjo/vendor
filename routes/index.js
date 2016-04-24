88435a185cc85c8f48b996f1437388a9ba14935c
Nematullah Razmjo <nimatullah.razmjo@gmail.com>
2016-04-24 13:29:43 +0430

    Change on routing files

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,10 @@
-/* GET home page. */
+/* GET home page.
+ *
+  *     @author : Nimatullah Razmjo < nimatullah.razmjo@gmail.com>
+  *     @date : April 22, 2016
+  *     @version : 1.0.0
+  *
+  * */
 module.exports = function(app) {
   app.get('*', function (req, res, next) {
     res.render('layout/index', {title: 'Vendor Registeration'});
